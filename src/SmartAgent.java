5519c2b657ee38403424b2da4c25180b70451bfc
Federico D'Ambrosio <fedexist@gmail.com>
2017-03-01 14:21:23 +0100

    Add agenda creation for users Add info generation for smartagent Add Pair implementation Add Icalendar retrieval for smartagent Change language level

--- src/SmartAgent.java
+++ src/SmartAgent.java
@@ -1,10 +1,15 @@
+import biweekly.Biweekly;
 import biweekly.ICalendar;
-import org.apache.commons.lang3.tuple.Pair;
+import org.apache.jena.datatypes.RDFDatatype;
+import org.apache.jena.graph.Node;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.sparql.vocabulary.FOAF;
+import org.apache.jena.util.FileManager;
 import org.apache.jena.vocabulary.RDF;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -19,14 +24,18 @@ public class SmartAgent {
     private ArrayList<String> contacts;
 
 
-    private ArrayList<Pair<Property, Resource>> personalPreferences;
-    private HashMap<String, Pair<Property, Resource> > contactsPreferences; // <contactID, Preferences>
+    private ArrayList< Pair<Property, RDFNode> > personalPreferences;
+    private HashMap<String, Pair<Property, RDFNode> > contactsPreferences; // <contactID, Preferences>
     private HashMap<String, ICalendar> agenda; // <meetingID, ICalObject>
 
     public SmartAgent(String ID, Dataset dataset) {
 
         delegateID = ID;
         ContactsProfileAgenda = dataset;
+        contacts = new ArrayList<String>();
+        personalPreferences = new ArrayList<Pair<Property, RDFNode>>();
+        contactsPreferences = new HashMap<String, Pair<Property, RDFNode>>();
+        agenda = new HashMap<String, ICalendar>();
 
         generatePersonalInfo();
 
@@ -35,7 +44,7 @@ public class SmartAgent {
     private void generatePersonalInfo() {
 
         //Contacts: from dataset
-        Model model = ContactsProfileAgenda.getDefaultModel();
+        Model model = ContactsProfileAgenda.getNamedModel("Contacts");
         StmtIterator stmtIt = model.listStatements(null, RDF.type, FOAF.Person);
 
         for( ; stmtIt.hasNext(); ){
@@ -45,17 +54,50 @@ public class SmartAgent {
 
         }
 
-
         //Personal preferences: from dataset
+        model = ContactsProfileAgenda.getNamedModel("Profile");
+        stmtIt = model.listStatements();
 
-        //Contacts Preferences: Contacts -> Contacts' smart agents -> Contacts' Preferences
+        for( ; stmtIt.hasNext(); ){
 
+            Statement currentstmt = stmtIt.next();
+            personalPreferences.add( new Pair<Property, RDFNode>(currentstmt.getPredicate(), currentstmt.getObject()));
+
+        }
+        
+        //Agenda
+        model = ContactsProfileAgenda.getNamedModel("Agenda");
+        Property meetingID = model.createProperty("https://www.smartcontacts.com/ontology#meetingID");
+        NodeIterator nodeIt = model.listObjectsOfProperty(meetingID);
+        for( ; nodeIt.hasNext(); ){
+
+            RDFNode currentNode = nodeIt.next();
+            ICalendar ical = retrieveICal(currentNode.toString());
+            if(ical != null)
+                agenda.put(currentNode.toString(), retrieveICal(currentNode.toString()));
+
+        }
+
+    }
+
+    private ICalendar retrieveICal(String ICalUID) {
+
+        FileManager fm = FileManager.get();
+        InputStream icalFile = fm.open("./res/"+ delegateID + "/" + ICalUID + ".ics");
+        ICalendar iCalendar = null;
+        try{
+            iCalendar = Biweekly.parse(icalFile).first();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return iCalendar;
     }
 
     public String getPersonalID() {
         return delegateID;
     }
-    public ArrayList<Pair<Property, Resource>> getPersonalPreferences() {
+    public ArrayList< Pair<Property, RDFNode> > getPersonalPreferences() {
         return personalPreferences;
     }
 
--- src/TakeAPizzaTogether.java
+++ src/TakeAPizzaTogether.java
@@ -1,11 +1,72 @@
+import biweekly.ICalendar;
+import biweekly.component.VEvent;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.rdf.model.*;
+
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class TakeAPizzaTogether {
 
-    ArrayList<String> users;
-    ArrayList<SmartAgent> smartAgents;
+    static ArrayList<String> users = new ArrayList<String>();
+    static ArrayList<SmartAgent> smartAgents = new ArrayList<SmartAgent>();
+
+    static ArrayList<ICalendar> usersCalendars = new ArrayList<ICalendar>();
+    static HashMap<String, Dataset> usersDatasets = new HashMap<String, Dataset>(); // <userID, Dataset>
+
+    static String datasetURI = "https://www.smartcontacts.com/ontology";
+    static String NS = datasetURI + "#";
 
     public static void main(String[] args) {
+
+        //Valori di test
+        users.add("Edoardo");
+        users.add("Federico");
+
+        //Agenda TDB
+        //Inserisce nel dataset dell'utente un modello con nome "Agenda" a cui si può fare riferimento
+        for(String user : users){
+
+            Model agenda = ModelFactory.createDefaultModel();
+            Dataset dataset = usersDatasets.get(user);
+            dataset.addNamedModel("Agenda", agenda);
+
+        }
+
+        for(String user : users){
+
+            ICalendar ical = new ICalendar();
+            ical.setUid(UUID.randomUUID().toString());
+
+                VEvent event = new VEvent();
+                event.setDescription("Test Event for user " + user);
+                event.setUid(UUID.randomUUID().toString());
+
+            ical.addEvent(event);
+
+            usersCalendars.add(ical);
+
+            Dataset currentDataset = usersDatasets.get(user);
+            Model agenda = currentDataset.getNamedModel("Agenda");
+
+            Resource subject = agenda.createResource(user);
+            Property predicate = agenda.createProperty(NS, "hasCalendar");
+            Resource object = agenda.createResource(ical.getUid().toString());
+            //Aggiunta tripla del calendar nel TDB
+            agenda.add(subject, predicate, object);
+
+            try(PrintWriter writer = new PrintWriter("./res/" + user + "/" + ical.getUid().toString() + ".ics", "UTF-8")){
+
+                ical.write(writer);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
 
     }
 
